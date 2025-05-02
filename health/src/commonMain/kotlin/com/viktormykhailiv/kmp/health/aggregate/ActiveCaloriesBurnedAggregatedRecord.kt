fb5c0b7795eaa44aac556c1539e231d3c9bee42d
Dominik Kluske <dome.k28@yahoo.com>
2025-05-02 21:09:23 +0200

    add documentation

--- health/src/commonMain/kotlin/com/viktormykhailiv/kmp/health/aggregate/ActiveCaloriesBurnedAggregatedRecord.kt
+++ health/src/commonMain/kotlin/com/viktormykhailiv/kmp/health/aggregate/ActiveCaloriesBurnedAggregatedRecord.kt
@@ -6,6 +6,10 @@ import com.viktormykhailiv.kmp.health.HealthDataType.ActiveCaloriesBurned
 import com.viktormykhailiv.kmp.health.IntervalRecord
 import kotlinx.datetime.Instant
 
+/**
+ * Captures the aggregated number of burned calories
+ * @param total The total aggregated number of burned calories
+ */
 class ActiveCaloriesBurnedAggregatedRecord(
     override val startTime: Instant,
     override val endTime: Instant,
--- health/src/commonMain/kotlin/com/viktormykhailiv/kmp/health/records/ActiveCaloriesBurnedRecord.kt
+++ health/src/commonMain/kotlin/com/viktormykhailiv/kmp/health/records/ActiveCaloriesBurnedRecord.kt
@@ -5,6 +5,10 @@ import com.viktormykhailiv.kmp.health.HealthDataType.ActiveCaloriesBurned
 import com.viktormykhailiv.kmp.health.IntervalRecord
 import kotlinx.datetime.Instant
 
+/**
+ * Captures the total calories burned
+ * @param total The total amount of calories burned. Must be >= 0
+ */
 class ActiveCaloriesBurnedRecord(
     override val startTime: Instant,
     override val endTime: Instant,
